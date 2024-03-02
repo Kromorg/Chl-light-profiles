ee4790daae6fd340b68bc4f0fd7d7482e42437b0
Manuel F. Velasco-Lozano <130931792+Kromorg@users.noreply.github.com>
2024-03-02 11:18:08 -0600

    opening netcdf files

--- main_code.R
+++ main_code.R
@@ -7,3 +7,14 @@ pacman:: p_load(tidyverse, # Data wrangling
 # Clean console ####
 rm(list = ls())
 shell('cls')
+
+# NetCDF files ####
+# Object with the name of the file that will be used
+ncfile<- '.nc'
+
+# Open NetCDF file
+cmes_data <- nc_open(ncfile)
+
+# File information
+print(cmes_data)
+attributes(cmes_data$var)
