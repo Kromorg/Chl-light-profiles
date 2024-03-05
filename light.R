11dd70ac4ce884673e3b1d42c98ef12fbdf0c73f
Manuel F. Velasco-Lozano <130931792+Kromorg@users.noreply.github.com>
2024-03-05 11:49:24 -0600

    add light percentage at La Ballena

--- light.R
+++ light.R
@@ -330,3 +330,48 @@ graph.lobos<- ggplot()+
         legend.title = element_text(size = 17))
 
 graph.lobos
+
+
+# La Ballena ####
+# February
+k <- monthly.summary %>% filter(Month == 'February' &
+                              Site == 'La Ballena') %>% 
+     dplyr:: select(Mean_k)
+z <- seq(from = 0, to = 100, by = 1)
+p <- seq(from = 100, to = 0, by = -1)
+feb <- data.frame(Depth = z, Month = 'February',
+                 Irradiance = p*exp(-k[[2]]*z))
+
+# September
+k <- monthly.summary %>% filter(Month == 'September' &
+                              Site == 'La Ballena') %>% 
+     dplyr:: select(Mean_k)
+z <- seq(from = 0, to = 100, by = 1)
+p <- seq(from = 100, to = 0, by = -1)
+sept <- data.frame(Depth = z, Month = 'September',
+                 Irradiance = p*exp(-k[[2]]*z))
+
+# Merge and create graphical object
+light.ballena <- rbind(feb, sept)
+
+graph.ballena<- ggplot()+
+  geom_jitter(data = light.ballena,
+              aes(x = Irradiance, y = Depth, fill = Month),
+              shape = 21, size = 3, alpha = 0.4,
+              show.legend = F)+
+  labs(y = 'Depth (m)', x = NULL)+
+  annotate('text', x = 75, y = 25, label = 'D) La Ballena',
+           size = 5)+
+  scale_y_reverse()+
+  scale_x_discrete(position = 'top')+
+  scale_x_continuous(position = 'top')+
+  geom_vline(xintercept = c(10, 1, 0.1), lwd = 1.5,
+             colour = c('forestgreen', 'darkorange',
+                        'darkred'), linetype = 'dotted')+
+  theme_bw(base_size = 13)+
+  theme(panel.grid = element_blank(),
+        axis.line = element_blank(),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 17))
+
+graph.ballena
